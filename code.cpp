1f1ff1624ccac7eac27679ebe4215fb7ba7f212a
Mohammad Ali <md.ali.haque.2468@gmail.com>
2025-10-09 02:51:59 +0600

    array reverse

--- code.cpp
+++ code.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void reverseArray(int array[], int size) {
+    int start = 0, end = size - 1;
+
+    while (start < end)
+    {
+        swap (array[start], array[end]);
+        start ++;
+        end--;
+    }
+    cout << "The reverse array is : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    
+}
+
 
 int main(){
-
-    cout << "Bismillah" << endl;
+    int size;
+    cout << "Enter the size of an array : ";
+    cin >>size;
+    
+    int array[size];
+    cout << "Enter the values of the array : ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+    
+    reverseArray(array,size);
     return 0;
 }
