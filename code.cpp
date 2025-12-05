5b47cc26b9840471be7c317089b131e3ad29e454
Mohammad Ali <md.ali.haque.2468@gmail.com>
2025-12-05 20:02:36 +0600

    Update Cp

--- code.cpp
+++ code.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    string p;
-    cin >> p;
-    
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++){
+        if (s[i] == s[i + 1]){
+            count  ++;
+        }
+    }
+
+    cout << count << endl;
+
     return 0;
 }
+
+
