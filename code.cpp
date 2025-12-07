1d03915e46f21af7b5c8ea7f606abbf26c12a440
Mohammad Ali <md.ali.haque.2468@gmail.com>
2025-12-07 20:05:15 +0600

    Update Cp

--- code.cpp
+++ code.cpp
@@ -4,20 +4,31 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-
-    int count = 0;
-
-    for (int i = 0; i < n; i++){
-        if (s[i] == s[i + 1]){
-            count  ++;
+    int t;
+    cin >> t;
+    for ( int i = 0; i < t; i++){
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        
+        int count = 0;
+        for (int j = 0; j < n; j++){
+            if (s[j] == '0'){
+                count++;
+            }else {
+                for (int l = j+1; l < k+j; l++){
+                    if (s[l] == '1'){
+                        j = k;
+                        continue;
+                    }
+                }
+                j += k;
+            }
         }
-    }
 
-    cout << count << endl;
+        cout << count << endl; //1 11 3 01010010000  
+    }
 
     return 0;
 }
