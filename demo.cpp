3868828be38d5c9488bfa746cc3607c476f729ce
Mohammad Ali <md.ali.haque.2468@gmail.com>
2026-03-18 03:27:03 +0600

    edit code file

--- demo.cpp
+++ demo.cpp
@@ -10,16 +10,6 @@ int main(){
     for (int i = 0; i < t; i++){
         int n;
         cin >> n;
-
-        vector<long long> a(n);
-        long long num;
-
-        for (int j = 0; j < n; j++){
-            cin >> a[j];
-        }
-
-        num =* max_element(a.begin(), a.end());
-        cout << num*n << endl;
     }
 
     return 0;
